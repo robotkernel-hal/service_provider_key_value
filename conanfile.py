ce740c2e15d1e07bfb9c13b3957c4861d33857b8
Johannes Nix <johannes.nix@dlr.de>
2023-01-11 13:09:38 +0100

    add url to conanfile to reduce noise

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,7 @@ class MainProject(ConanFile):
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "service_provider_key_value"
+    url = "https://rmc-github.robotic.dlr.de/robotkernel/service_provider_key_value"
     description = "robotkernel service provider for key value devices."
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
 
